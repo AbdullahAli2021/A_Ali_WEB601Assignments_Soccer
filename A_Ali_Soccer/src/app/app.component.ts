7f475f9acb7214fe77f77e45084ce797fa6ced2f
Abdulah Ali <w0782917@myscc.ca>
2022-04-17 19:46:04 -0400

    assignmetnt

--- A_Ali_Soccer/src/app/app.component.ts
+++ A_Ali_Soccer/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { Content } from './helper-files/content-interface';
 import { LogUpdateService } from './log-update.service';
 import { SoccerService } from './services/soccer.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,15 @@ import { SoccerService } from './services/soccer.service';
 export class AppComponent {
   title = 'A_Ali_Soccer';
   getId?: Content;
-  constructor(private SoccerService: SoccerService, private logService: LogUpdateService ) {}
+  constructor(private SoccerService: SoccerService, private logService: LogUpdateService,private appRef: ApplicationRef,
+    // so we can check stability
+    private updates: SwUpdate
+    // so we can check for updates
+     ) {}
 
   ngOnInit(): void {
     this.logService.init();
+    this.updates.checkForUpdate();
 
 
 
